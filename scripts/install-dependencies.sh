67cc803c7f5fcc8cb4f040302037cd563c9a81f1
Arfin2me <121047040+Arfin2me@users.noreply.github.com>
2025-06-01 20:15:39 +0200

    Create install-dependencies.sh

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+echo "[+] Installing base dependencies..."
+
+apt-get update && \
+apt-get install -y \
+    curl \
+    gnupg \
+    apt-transport-https \
+    software-properties-common \
+    unixodbc-dev \
+    wget \
+    ca-certificates && \
+apt-get clean && \
+rm -rf /var/lib/apt/lists/*
