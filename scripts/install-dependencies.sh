078946d9f6da230e33b3cbe06322152d87acbaab
Arfin2me <121047040+Arfin2me@users.noreply.github.com>
2025-06-01 22:42:31 +0200

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 set -e
-echo "[+] Installing base dependencies..."
+
+echo "[+] Installing base dependencies and Microsoft repo..."
+
+# Add Microsoft GPG key and repository once
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/microsoft.gpg
+echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/20.04/prod focal main" > /etc/apt/sources.list.d/microsoft.list
 
 apt-get update && \
 apt-get install -y \
