64e3c469f51ef85110b9ee07d97fc2849bb8298b
Arfin2me <121047040+Arfin2me@users.noreply.github.com>
2025-06-04 13:52:38 +0200

    Update install-modules.sh

--- scripts/install-modules.sh
+++ scripts/install-modules.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
 set -e
+
 echo "[+] Installing PowerShell modules..."
 
+# Ensure module dir exists
+mkdir -p /usr/local/share/powershell/Modules
+
+# Install modules
 pwsh -Command "Install-Module dbatools -Force -Scope AllUsers"
 pwsh -Command "Install-Module SqlServer -Force -Scope AllUsers"
 
