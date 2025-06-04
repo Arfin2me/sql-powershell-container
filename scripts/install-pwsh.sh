5d2191f9b31758fd509dd2e1dad48d1fa18838f2
Arfin2me <121047040+Arfin2me@users.noreply.github.com>
2025-06-04 13:53:09 +0200

    Update install-pwsh.sh

--- scripts/install-pwsh.sh
+++ scripts/install-pwsh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 echo "[+] Installing PowerShell..."
 
@@ -7,10 +6,7 @@ echo "[+] Installing PowerShell..."
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/20.04/prod focal main" > /etc/apt/sources.list.d/microsoft.list
 
-# Install PowerShell
 apt-get update && \
-apt-get install -y powershell && \
-apt-get clean && \
-rm -rf /var/lib/apt/lists/*
-
-echo "[✓] PowerShell installed."
+    apt-get install -y powershell && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
