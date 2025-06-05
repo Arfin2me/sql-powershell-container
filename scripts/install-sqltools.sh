ff084571a575e21c3278105e7da65836078c4861
Arfin2me <121047040+Arfin2me@users.noreply.github.com>
2025-06-05 23:09:55 +0200

    Update install-sqltools.sh

--- scripts/install-sqltools.sh
+++ scripts/install-sqltools.sh
@@ -8,9 +8,11 @@ curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sou
 
 apt-get update && \
     ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev && \
-    echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc && \
-    echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.profile && \
+    echo 'export PATH="$PATH:/opt/mssql-tools/bin"' > /etc/profile.d/sqltools.sh && \
+    chmod +x /etc/profile.d/sqltools.sh && \
     ln -sf /opt/mssql-tools/bin/sqlcmd /usr/local/bin/sqlcmd && \
-    ln -sf /opt/mssql-tools/bin/bcp /usr/local/bin/bcp
+    ln -sf /opt/mssql-tools/bin/bcp /usr/local/bin/bcp && \
+    ln -sf /opt/mssql-tools/bin/sqlcmd /usr/bin/sqlcmd && \
+    ln -sf /opt/mssql-tools/bin/bcp /usr/bin/bcp
 
 echo "[✓] SQL tools installed."
