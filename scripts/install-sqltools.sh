863a18b484286d4235343f6bf14dbde63272c769
Arfin2me <121047040+Arfin2me@users.noreply.github.com>
2025-06-05 23:53:25 +0200

    Update install-sqltools.sh

--- scripts/install-sqltools.sh
+++ scripts/install-sqltools.sh
@@ -7,7 +7,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 apt-get update && \
-    ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev && \
+    ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools unixodbc-dev && \
     echo 'export PATH="$PATH:/opt/mssql-tools/bin"' > /etc/profile.d/sqltools.sh && \
     chmod +x /etc/profile.d/sqltools.sh && \
     ln -sf /opt/mssql-tools/bin/sqlcmd /usr/local/bin/sqlcmd && \
