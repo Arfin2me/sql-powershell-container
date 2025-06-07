59aa861c41a3726ed49d87bd8e97720290c7a993
Arfin2me <bartekkruk99@gmail.com>
2025-06-07 12:16:20 +0200

    Delete connect-success.sql

--- connect-success.sql
+++
@@ -1,6 +0,0 @@
--- connect-success.sql
--- Demonstrates using the :CONNECT meta-command in sqlcmd
--- Change the connection details as needed for your environment
-:CONNECT localhost,1433
-SELECT @@SERVERNAME AS ConnectedTo;
-GO
